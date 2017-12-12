bdd3ebd64f7b7221640b25659e72c141bd39f423
pandadezzz <cheno1201@gmail.com>
2017-12-12 11:00:16 -0500

    update

--- test.php
+++ test.php
@@ -18,6 +18,17 @@ $username = $_SESSION["loginUsername"];
 	$row=mysqli_fetch_assoc($result);
 	echo $row['loginkey']."<br>";
 	
+	if(isset($_COOKIE['loginkeycookie'])){
+		 echo"cookie exists"."<br>";
+	if($_COOKIE['loginkeycookie'] != $row['loginkey']){
+	  echo"it doesn't match"."<br>";
+	  }
+	}
+	
+	if(!(isset($_COOKIE['loginkeycookie']) && $_COOKIE['loginkeycookie'] == $row['loginkey'])){
+	  echo"it doesn't match"."<br>";
+	  }
+	
 echo $_COOKIE['loginkeycookie'];
 echo "<br>";
 //echo $_SESSION["loginUsername"];
